6c511cac50e32d579b146e8834d0e6bb3d703059
ivenckunas <ignas.venckunas@gmail.com>
2022-07-14 16:35:58 +0300

    gaunam formos lauku value i object

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 "use strict";
 
-console.log("main.js");
+const htmlEls = {
+  form: document.forms[0],
+  title: document.getElementById("title"),
+  image: document.getElementById("image"),
+  date: document.getElementById("date"),
+  text: document.getElementById("text"),
+  author: document.getElementById("author"),
+  submit: document.getElementById("submit"),
+};
+
+htmlEls.form.addEventListener("submit", myFunction);
+
+function myFunction(event) {
+  event.preventDefault();
+
+  const newPostObj = {
+    title: htmlEls.title.value,
+    image: htmlEls.image.value,
+    date: htmlEls.date.value,
+    text: htmlEls.text.value,
+    author: htmlEls.author.value,
+  };
+  console.log(newPostObj);
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Posts form
+
+1. Create a new form.
+2. Title, Image(url), Date, Body(textarea), Author(select)
+3. Take all form values into object
+4. Make post from data element in HTML
+5. Clear form values
+6. If post is resubmited we get the another post into HTML
