87adc964a979c3ed99d85175ea9df1b5bb15758f
ivenckunas <ignas.venckunas@gmail.com>
2022-07-14 17:00:53 +0300

    new post function with innerHTML

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ const htmlEls = {
   submit: document.getElementById("submit"),
 };
 
+const postContainerEl = document.querySelector(".posts-container");
+
 htmlEls.form.addEventListener("submit", myFunction);
 
 function myFunction(event) {
@@ -23,4 +25,28 @@ function myFunction(event) {
     author: htmlEls.author.value,
   };
   console.log(newPostObj);
+  makePostToHtml(newPostObj, postContainerEl);
+  htmlEls.form.reset();
+  // arba this.reset()
 }
+
+function makePostToHtml(post, dest) {
+  const articleEl = document.createElement("div");
+  articleEl.className = "post card";
+  articleEl.innerHTML = `
+    <img src="${post.image}" alt="post picture" />
+    <h3 class="post__title">${post.title}</h3>
+    <h4 class="post__author">${post.author}</h4>
+    <p class="post__date">${post.date}</p>
+    <p class="post__text">${post.text}</p>
+    `;
+  dest.append(articleEl);
+}
+
+//   <article class="post card">
+//     <img src="https://picsum.photos/id/1016/1000/800" alt="post picture" />
+//     <h3 class="post__title">Title</h3>
+//     <h4 class="post__author">Autorius</h4>
+//     <p class="post__date">Data</p>
+//     <p class="post__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia eaque ipsam alias sed repellat illo cupiditate nulla praesentium corporis incidunt!</p>
+//   </article>;
